1d9b9629ddce73c1ac44aa36202ea36fc7367b0b
vara-prasad-dhara <svaraprasad86@hotmail.com>
2023-03-09 21:20:50 +0530

    TAL : integrated with .net core api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MonthlyPremiumComponent } from './monthly-premium/monthly-premium.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrComponent } from './toastr/toastr.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MonthlyPremiumComponent
+    MonthlyPremiumComponent,
+    PageNotFoundComponent,
+    ToastrComponent
   ],
   imports: [
     BsDatepickerModule.forRoot(),
@@ -20,6 +25,7 @@ import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
     FormsModule,
     ReactiveFormsModule,
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
